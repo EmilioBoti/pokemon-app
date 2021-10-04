0595368ea9a4d7c8d8a63782d0d240d8e6c6bdee
emilio <emiliobotier@gmail.com>
2021-10-04 13:03:44 +0200

    second commit

--- app/src/main/java/com/example/pokemonapp/fragments/PokemonFragment.java
+++ app/src/main/java/com/example/pokemonapp/fragments/PokemonFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
@@ -12,12 +13,15 @@ import com.example.pokemonapp.R;
 
 public class PokemonFragment extends Fragment {
     private ImageView imageView;
+    private ImageButton goBack;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         View view = inflater.inflate(R.layout.fragment_pokemon, container, false);
 
         imageView = view.findViewById(R.id.image);
+        goBack = view.findViewById(R.id.goBack);
         //imageView.setImageResource(R.mipmap.ic_launcher_raichu_round);
         return view;
     }
