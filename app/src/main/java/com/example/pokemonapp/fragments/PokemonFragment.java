e5a96c97d6d2c8469d7ca164e833617b0ea6c3ba
emilio <emiliobotier@gmail.com>
2021-10-07 00:16:17 +0200

    input Search working

--- app/src/main/java/com/example/pokemonapp/fragments/PokemonFragment.java
+++ app/src/main/java/com/example/pokemonapp/fragments/PokemonFragment.java
@@ -34,28 +34,38 @@ public class PokemonFragment extends Fragment {
     private ImageButton goBack;
     private TextView namePoke;
     private LinearLayout linearLayout, expPoke;
-    public String name;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         View view = inflater.inflate(R.layout.fragment_pokemon, container, false);
 
-        linearLayout = view.findViewById(R.id.typePokemon);
-        expPoke = view.findViewById(R.id.expPoke);
+        //get all views
+        getViews(view);
 
-        namePoke = view.findViewById(R.id.namePoke);
-        imageView = view.findViewById(R.id.image);
-        goBack = view.findViewById(R.id.goBack);
-
-        OkHttpClient client = new OkHttpClient();
 
         Bundle data = getArguments();
         String input = data.getString("data");
+        getNamePoke(input);
 
+        //event to go back
+        goBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view1){
+                getActivity().onBackPressed();
+            }
+        });
+
+        return view;
+    }
+
+    private void getNamePoke(String input){
+        OkHttpClient client = new OkHttpClient();
         Services services = new Services();
 
         try {
             Request request = services.getPokemon(input);
+
             client.newCall(request).enqueue(new Callback() {
                 @Override
                 public void onFailure(@NonNull Call call, @NonNull IOException e) {
@@ -88,21 +98,16 @@ public class PokemonFragment extends Fragment {
                 }
             });
 
-
-
         }catch (IOException | JSONException e){
 
         }
-        //Toast.makeText(getContext(), input, Toast.LENGTH_SHORT).show();
 
-        //event to go back
-        goBack.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view1){
-                getActivity().onBackPressed();
-            }
-        });
-
-        return view;
+    }
+    private void getViews(View view){
+        linearLayout = view.findViewById(R.id.typePokemon);
+        expPoke = view.findViewById(R.id.expPoke);
+        namePoke = view.findViewById(R.id.namePoke);
+        imageView = view.findViewById(R.id.image);
+        goBack = view.findViewById(R.id.goBack);
     }
 }
