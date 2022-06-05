4d22b179e83e5c2e495166e0bab9fc666c8f3334
Emilio Botier <luisemilio.rodriguezbotier.st@everis.nttdata.com>
2022-06-05 12:33:36 +0200

    added binding

--- app/src/main/java/com/example/pokemonapp/view/ui/fragments/PokemonFragment.java
+++ app/src/main/java/com/example/pokemonapp/view/ui/fragments/PokemonFragment.java
@@ -35,6 +35,7 @@ import com.example.pokemonapp.businessLogic.pokeDetails.DetailPresenter;
 import com.example.pokemonapp.businessLogic.pokeDetails.DetailProvider;
 import com.example.pokemonapp.businessLogic.pokeDetails.IDetail;
 import com.example.pokemonapp.components.EvolutionContainer;
+import com.example.pokemonapp.databinding.FragmentPokemonBinding;
 import com.example.pokemonapp.models.Pokemon;
 import com.example.pokemonapp.utils.common.OnClickItemListener;
 import com.example.pokemonapp.utils.constants.Helpers;
@@ -47,6 +48,7 @@ import java.util.ArrayList;
 
 
 public class PokemonFragment extends Fragment implements IDetail.ViewPresenter, OnClickItemListener {
+    private FragmentPokemonBinding binding;
     private ImageView pokeImage;
     private ImageButton goBack;
     private TextView namePoke, pokeDescription, titleBar;
@@ -60,8 +62,9 @@ public class PokemonFragment extends Fragment implements IDetail.ViewPresenter, 
     private EvolutionContainer evolutionContainer;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
-        return inflater.inflate(R.layout.fragment_pokemon, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
+        binding = FragmentPokemonBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
@@ -69,8 +72,8 @@ public class PokemonFragment extends Fragment implements IDetail.ViewPresenter, 
         super.onViewCreated(view, savedInstanceState);
 
         init(view);
-        scrollViewContainer.setVisibility(View.GONE);
-        progressBar.setVisibility(View.VISIBLE);
+        binding.scrollViewContainer.setVisibility(View.GONE);
+        binding.loader.setVisibility(View.VISIBLE);
 
         titleBar.setText(R.string.pokeInfo);
 
@@ -88,24 +91,10 @@ public class PokemonFragment extends Fragment implements IDetail.ViewPresenter, 
 
     }
 
-    private void init(View view){
-        typesView = view.findViewById(R.id.typePokemon);
-        abilities = view.findViewById(R.id.abilitiesPoke);
-        weight = view.findViewById(R.id.weight);
-        height = view.findViewById(R.id.height);
-        baseExp = view.findViewById(R.id.expPoke);
-        namePoke = view.findViewById(R.id.namePoke);
-        pokeImage = view.findViewById(R.id.pokeImage);
-        evolutions = view.findViewById(R.id.evolutions);
+    private void init(View view) {
         goBack = view.findViewById(R.id.goBack);
         titleBar = view.findViewById(R.id.titleBar);
-        pokeDescription = view.findViewById(R.id.pokeDescription);
-        habitatLayout = view.findViewById(R.id.habitat);
-        progressBar = view.findViewById(R.id.loader);
-        scrollViewContainer = view.findViewById(R.id.scrollViewContainerr);
-        descriptionMainContainer = view.findViewById(R.id.descriptionMainContainer);
         goBackContainer = view.findViewById(R.id.goBackContainer);
-        evolutionContainer = view.findViewById(R.id.evoContainer);
         model = new DetailProvider();
         presenter = new DetailPresenter(this, model);
     }
@@ -123,11 +112,11 @@ public class PokemonFragment extends Fragment implements IDetail.ViewPresenter, 
     }
 
     private void evolutionAdapter(Context context, ArrayList<Pokemon> listPokemon) {
-        evolutionContainer.setAdapter(listPokemon);
+       binding.evoContainer.setAdapter(listPokemon);
     }
 
     private void updateColor(int color) {
-        descriptionMainContainer.setCardBackgroundColor(color);
+        binding.descriptionMainContainer.setCardBackgroundColor(color);
         goBackContainer.setBackgroundColor(color);
 
         if (getActivity() != null) {
@@ -152,57 +141,60 @@ public class PokemonFragment extends Fragment implements IDetail.ViewPresenter, 
     @Override
     public void setEvolutions(ArrayList<Pokemon> list) {
         callAdapter(getContext(), list);
-        evolutionContainer.setListener(this.presenter);
+        binding.evoContainer.setListener(this.presenter);
     }
 
     private void setContent(Pokemon pokemon) {
-        progressBar.setVisibility(View.GONE);
-        scrollViewContainer.setVisibility(View.VISIBLE);
+        binding.loader.setVisibility(View.GONE);
+        binding.scrollViewContainer.setVisibility(View.VISIBLE);
+        binding.scrollViewContainer.setSmoothScrollingEnabled(true);
+        binding.scrollViewContainer.setScrollY(0);
 
         final float DIMEN = 16f;
-        pokeImage.setTranslationY(-1000f);
+        binding.pokeImage.setTranslationY(-1000f);
         //load the image
-        Helpers.loadImage(pokemon.getSpriteFront(), pokeImage);
+        Helpers.loadImage(pokemon.getSpriteFront(), binding.pokeImage);
+        settingColor();
         //animation
-        ObjectAnimator animatorImg = ObjectAnimator.ofFloat(pokeImage, "translationY", 0f);
+        ObjectAnimator animatorImg = ObjectAnimator.ofFloat(binding.pokeImage, "translationY", 0f);
         animatorImg.setDuration(600);
         animatorImg.start();
 
-        weight.removeAllViewsInLayout();
-        baseExp.removeAllViewsInLayout();
-        typesView.removeAllViewsInLayout();
-        abilities.removeAllViewsInLayout();
-        habitatLayout.removeAllViewsInLayout();
-        height.removeAllViewsInLayout();
-
-        setColor();
+        binding.weight.removeAllViewsInLayout();
+        binding.expPoke.removeAllViewsInLayout();
+        binding.typePokemon.removeAllViewsInLayout();
+        binding.abilitiesPoke.removeAllViewsInLayout();
+        binding.habitat.removeAllViewsInLayout();
+        binding.height.removeAllViewsInLayout();
 
         double num = (pokemon.getWeight() / 4.54);
         String pound = String.format("%.02f", num);
 
         String n = pokemon.getName().replaceAll("-", " ");
-        pokeDescription.setText(pokemon.getDescription());
-        namePoke.setText(Helpers.ToUpperName(n));
-        habitatLayout.addView(createTextView(pokemon.getHabitat(), DIMEN));
-        weight.addView(createTextView(pound+" pounds", DIMEN));
-        height.addView(createTextView((pokemon.getHeight() * 10) + " cm", DIMEN));
-        baseExp.addView(createTextView(String.valueOf(pokemon.getBaseExperience()), DIMEN));
+        binding.pokeDescription.setText(pokemon.getDescription());
+        binding.namePoke.setText(Helpers.ToUpperName(n));
+        binding.habitat.addView(createTextView(pokemon.getHabitat(), DIMEN));
+        binding.weight.addView(createTextView(pound+" pounds", DIMEN));
+        binding.height.addView(createTextView((pokemon.getHeight() * 10) + " cm", DIMEN));
+        binding.expPoke.addView(createTextView(String.valueOf(pokemon.getBaseExperience()), DIMEN));
 
-        getElem(pokemon.getTypes(), typesView, DIMEN);
-        getElem(pokemon.getAbilities(), abilities, DIMEN);
+        getElem(pokemon.getTypes(), binding.typePokemon, DIMEN);
+        getElem(pokemon.getAbilities(), binding.abilitiesPoke, DIMEN);
     }
 
-    private void setColor() {
-        Bitmap bitmap = ((BitmapDrawable) pokeImage.getDrawable()).getBitmap();
-        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
-            @Override
-            public void onGenerated(@Nullable Palette palette) {
-                Palette.Swatch vibrant = palette.getLightVibrantSwatch();
-                if (vibrant != null) {
-                    updateColor(vibrant.getRgb());
+    private void settingColor() {
+        if (binding.pokeImage.getDrawable() != null){
+            Bitmap bitmap = ((BitmapDrawable) binding.pokeImage.getDrawable()).getBitmap();
+            Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                @Override
+                public void onGenerated(@Nullable Palette palette) {
+                    Palette.Swatch vibrant = palette.getLightVibrantSwatch();
+                    if (vibrant != null) {
+                        updateColor(vibrant.getRgb());
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 
     private void getElem(ArrayList<String> type, LinearLayout layout , Float dimen) {
@@ -219,8 +211,8 @@ public class PokemonFragment extends Fragment implements IDetail.ViewPresenter, 
 
     @Override
     public void onClickItem(int post) {
-        scrollViewContainer.setVisibility(View.GONE);
-        progressBar.setVisibility(View.VISIBLE);
+        binding.scrollViewContainer.setVisibility(View.GONE);
+        binding.loader.setVisibility(View.VISIBLE);
         presenter.callPoke(String.valueOf(post));
     }
 
